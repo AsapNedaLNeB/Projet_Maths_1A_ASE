490e68837dc008e6a23af54c5ef58f4708fefceb
Erwan Sauzay <erwan.sauzay@uha.fr>
2024-05-30 17:20:40 +0200

    j ai un bug de simulation nrj inf .. XD

--- Methode_Euler_ordre_2.py
+++ Methode_Euler_ordre_2.py
@@ -4,8 +4,8 @@ Projet Maths 1A ASE Mai 2024
 Resolution de l'equation du deuxieme ordre (E) par la méthode d'Euler :
 Cette méthode donne une approximation numérique du résultat 
 
-(E): ddT + (g/l)*sin(T)  avec T(0) = y0 et dT(0) =dT0
-On peut poser dT(x) = F(dT(x),T(x),x) = -(g/l)*sin(T)
+(E): ddTheta + (g/l)*sin(Theta)  avec Theta(0) = y0 et dTheta(0) =dTheta0
+On peut poser dTheta(x) = F(dT(x),Theta(x),x) = -(g/l)*sin(Theta)
 """
 
 import numpy as np
@@ -13,36 +13,45 @@ import matplotlib.pyplot as plt
 
 # Constantes :
  
-g = 9,8 # m/s**2
+g = 9.8 # m/s**2
 l = 20  # m
-T0 = 20 #rad
-dT0 = 8 #rad/s
+Theta0 = 0 #rad
+dTheta0 = 5 #rad/s
 
 # Paramétres d'étude : 
 
-[x0,xf] = [0,10] #Interval d'étude
+[t0,tf] = [0,100] #Interval d'étude
 N = 1000         #Discretisation
-h = (xf-x0)/N    #Pas d'étude
+h = (tf-t0)/N    #Pas d'étude
 
 # L'erreur commise est en °(h**2)
 
 # La suite de solution discrétisé à °(h**2) prés: 
 
-T = np.zeros(N)
-dT = np.zeros(N)
-x = np.zeros(N)
-T[0]  = T0
-dT[0] = dT0
-x[0]  = x0
+Theta = np.zeros(N)
+dTheta = np.zeros(N)
+t = np.zeros(N)
+Theta[0]  = Theta0
+dTheta[0] = dTheta0
+t[0]  = t0
 
 for i in range (0,N-1):
-    x[i+1] = x[i] + h
+    t[i+1] = t[i] + h
+    Theta[i+1] = Theta[i] +h*dTheta[i]
+    dTheta[i+1] = dTheta[i] - h*(g/l)*np.sin(Theta[i])
    
 
 plt.close()
 plt.figure(2)
-plt.title("")
-plt.xlabel("x")
-plt.ylabel("")
-plt.plot(x)
+plt.title("Position angulaire du pendule simple au cour du temps")
+plt.xlabel("t")
+plt.plot(t,Theta)
+plt.ylabel("Theta")
+
+plt.figure(3)
+plt.title("Vitesse angulaire du pendule simple au cours du temps")
+plt.xlabel("t")
+plt.plot(t,dTheta)
+plt.ylabel("dTheta")
+
 plt.show()
